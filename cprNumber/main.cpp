3795cb2b9610e430a1755ca94adb0c284539e13e
CrimsonTome <crimsontome427@protonmail.com>
2023-02-07 01:35:07 +0000

    chore: add explanation of ascii magic

--- cprNumber/main.cpp
+++ cprNumber/main.cpp
@@ -10,6 +10,13 @@ int main(){
     int con = 0;
     int mult[] = {4,3,2,7,6,5,4,3,2,1};
     for (int i = 0; i< cprNum.length(); ++i){
+        // ASCII magic to convert a char of an int to its int value
+        // this will only work for values 0-9
+        // cprNum[i] holds the current character value of the integers we get from the input
+        // let's substitute in a value for cprNum[i] as an example
+        // '0' in ASCII is 48
+        // we'll use the number 3, its ASCII value is 51
+        // '3' - '0' = 51 - 48 = 3 
         con = cprNum[i] - '0';
         sum += con * mult[i];
     }
